c3199fdbc04e3552ce68891132374d20b57e3983
Brad Slayter <BradleySlayter@my.unt.edu>
2015-06-15 13:43:29 -0500

    Initial Commit

---
+++ DeafShark/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DeafShark
+//
+//  Created by Bradley Slayter on 6/15/15.
+//  Copyright © 2015 Flipped Bit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
